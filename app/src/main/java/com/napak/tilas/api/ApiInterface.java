808d68124c61b6b045147f51742928239cbdf9de
Lutfiyanti <fiyanti33@gmail.com>
2021-01-10 21:38:13 +0700

    add apiinterface

--- app/src/main/java/com/napak/tilas/api/ApiInterface.java
+++ app/src/main/java/com/napak/tilas/api/ApiInterface.java
@@ -1,8 +1,11 @@
 package com.napak.tilas.api;
 
 import com.napak.tilas.model.LumenResponse;
+import com.napak.tilas.model.Photo;
 import com.napak.tilas.model.PhotoListItem;
 import com.napak.tilas.model.PhotoMap;
+
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.*;
 
@@ -12,4 +15,12 @@ public interface ApiInterface {
 
     @GET("/photos/map")
     Call<List<PhotoMap>> listPhotoMap();
+
+    @POST("/photo")
+
+    @GET("/photos")
+    Call<List<PhotoListItem>> listPhotos();
+
+    @GET("/photo/{id}")
+    Call<List<Photo>> listPhoto();
 }
