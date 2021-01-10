7f852bc0cb63d60b5bfafbe919ca48729c816c68
Lutfiyanti <fiyanti33@gmail.com>
2021-01-10 14:41:45 +0700

    change first activity

--- app/src/main/java/com/napak/tilas/StartActivity.java
+++ app/src/main/java/com/napak/tilas/StartActivity.java
@@ -2,13 +2,31 @@ package com.napak.tilas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    Button mulai;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button btn = (Button) findViewById(R.id.mulai);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(StartActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }
