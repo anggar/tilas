7de5e37e74026b6bf48f13780334e0b86010803e
anggar <anggarwahyu@gmail.com>
2021-01-11 10:43:29 +0700

    Current loc in Maps activity

--- app/src/main/java/com/napak/tilas/MapsActivity.java
+++ app/src/main/java/com/napak/tilas/MapsActivity.java
@@ -1,5 +1,15 @@
 package com.napak.tilas;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -7,6 +17,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -16,16 +28,69 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final String LONGITUDE = "LNG";
     private GoogleMap mMap;
 
+    private LocationManager locationManager;
+    private MyLocationListener locationListener;
+    private boolean locationPermissionGranted;
+
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 112;
+
+    private class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            double myLat = location.getLatitude();
+            double myLng = location.getLongitude();
+
+            LatLng myLoc = new LatLng((float) myLat, (float) myLng);
+            mMap.addMarker(new MarkerOptions()
+                    .position(myLoc)
+                    .title("Your Location")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+        }
+    }
+
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        }
+        else {
+            ActivityCompat.requestPermissions( this,
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        getLocationPermission();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        locationListener = new MyLocationListener();
+        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        startLocationRequest();
     }
 
+    private void startLocationRequest() {
+        if (!locationPermissionGranted) {
+            getLocationPermission();
+        }
+
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                3000, 2, locationListener);
+    }
+    
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -40,8 +105,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(lat, lng);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng loc = new LatLng(lat, lng);
+        mMap.addMarker(new MarkerOptions().position(loc).title("Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+
     }
 }
